7fd0782eb0f01c422a76843315b1db1acd25f5f3
magendreau <32451540+magendreau@users.noreply.github.com>
2017-11-11 20:07:07 +0100

    Update pythonScript.py

--- pythonScript.py
+++ pythonScript.py
@@ -16,5 +16,5 @@ data2= select2.transpose()
 ax2 = fig.add_subplot(212)
 n, bins, patches = ax2.hist(0.1*data2[0], 1000, normed=0, alpha=0.75)
 ax2.set_xlim([400, 500])
-ax2.set_ylim([0, 800])
+ax2.set_ylim([0, 1000])
 pl.show();
